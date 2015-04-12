046893de2f310fb7da5d5f8f14d588ac21a248c9
james <james1071@gmail.com>
2015-04-12 11:04:15 +0100

    password added

---
+++ 3.3.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(define (make-account balance secret-password)
+  (define (withdraw amount  )
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount  )
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch password m)
+    (cond ((not (eq? password secret-password )) (error "Incorrect password"))
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+((acc 'secret-password 'withdraw) 40)
+
+((acc 'some-other-password 'deposit) 50)
